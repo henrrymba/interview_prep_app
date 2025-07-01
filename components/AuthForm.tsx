fb11c852f3fa40f8c5e418ceb254efdf6b493bd9
Henrry <YOUR_EMAIL>
2025-07-01 11:25:49 +0200

    feat(auth): add account creation and login UI
    Implement UI for user registration and login forms.

---
+++ components/AuthForm.tsx
@@ -0,0 +1,108 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import { Form } from "@/components/ui/form"
+import Image from "next/image";
+import Link from "next/link";
+import {toast} from "sonner";
+import FormField from "@/components/FormField";
+import {useRouter} from "next/navigation";
+
+
+const authFormSchema = (type: FormType) => {
+    return z.object({
+        name: type === 'sign-up' ? z.string().min(3) : z.string().optional(),
+        email: z.string().email(),
+        password: z.string().min(3),
+    })
+}
+
+const AuthForm = ({type}: {type: FormType}) => {
+    const router = useRouter()
+    const formSchema = authFormSchema(type);
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+            email: "",
+            password: "",
+        },
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        try{
+            if(type === "sign-up"){
+                toast.success('Cuenta creada con exito, por favor, inicia sesión.')
+                router.push("/sign-in")
+            } else {
+                toast.success('Inicio de sesión exitoso.')
+                router.push("/")
+            }
+        } catch (error){
+            console.log(error);
+            toast.error(`There was an error ${error}`);
+        }
+        console.log(values)
+    }
+
+    const isSignIn = type === "sign-in";
+
+    return (
+        <div className="card-border lg:min-w-[566px]">
+            <div className="flex flex-col gap-6 card py-14 px-10">
+                <div className="flex flex-row gap-2 justify-center">
+                    <Image
+                        src="/logo.svg"
+                        alt="logo"
+                        height={32}
+                        width={38}
+                    />
+                    <h2 className="text-primary-100">PrepWise</h2>
+                </div>
+                <h3>Practica entrevistas de trabajo con IA</h3>
+
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6 mt-4 form">
+                        {!isSignIn && (
+                            <FormField
+                                control={form.control}
+                                name="name"
+                                label="Nombre"
+                                placeholder="Introduce tu nombre"
+                                type="text"
+                            />
+                        )}
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            label="Correo Electrónico"
+                            placeholder="Introduce tu correo electrónico"
+                            type="email"
+                        />
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            label="Contraseña"
+                            placeholder="Introduce una contraseña"
+                            type="password"
+                        />
+
+                        <Button className="btn" type="submit">{isSignIn ? 'Iniciar Sesión' : 'Crear una cuenta'}</Button>
+                    </form>
+                </Form>
+
+                <p className="text-center">
+                    {isSignIn ? '¿Todavía no tienes cuenta?' : '¿Ya tienes una cuenta?'}
+                    <Link href={!isSignIn ? '/sign-in' : '/sign-up'} className="font-bold text-user-primary ml-1">
+                        {!isSignIn ? 'Inicia Sesión' : 'Crea una cuenta'}
+                    </Link>
+                </p>
+            </div>
+        </div>
+    )
+}
+export default AuthForm
